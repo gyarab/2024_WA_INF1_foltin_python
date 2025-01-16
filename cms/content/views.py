f10e7455401e9d02eeb3b6aa94f871d4a388aa0f
Ing. Kamil Foltin <kamil.foltin@gyarab.cz>
2025-01-16 11:29:34 +0100

    templates

--- cms/content/views.py
+++ cms/content/views.py
@@ -8,27 +8,12 @@ def articles(request):
     with open('articles.json', encoding="utf-8") as file:
         data = json.load(file) 
     
-    html_list = '<ul>'
-    id = 0
-    for article in data:
-        title = article['title']
-        url_article = reverse('content:article', args=[id])
-        html_list += f'<li><a href="{url_article}">{title}<a></li>'
-        id += 1
-    html_list += '</ul>'
-
-    return HTTPResponse(html_list)
+    return render(request, 'content/articles.html', {'data': data})
 
 def article(request, id):
     with open('articles.json', encoding="utf-8") as file:
         data = json.load(file)
-    
+     
     article = data[id]
-    title = article['title']
-    perex = article['perex']
-    image = article['image']
-
-    url_articles = reverse('content:articles')
-    back = f"<a href='{url_articles}'>Zpet na seznam</a>"
+    return render(request, 'content/article.html', {'article': article})
     
-    return HTTPResponse(f'<h1>{title}</h1><img src="{image}" alt="Article Image"><p>{perex}</p>'+back)
