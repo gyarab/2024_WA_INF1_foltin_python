f160d2497eba04c3fe5e3f4131cec115d1d019b1
Ing. Kamil Foltin <kamil.foltin@gyarab.cz>
2025-01-16 12:02:47 +0100

    templates

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'content.apps.ContentConfig', 
-    
 ]
 
 MIDDLEWARE = [
@@ -118,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
