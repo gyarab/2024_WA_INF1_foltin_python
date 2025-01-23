20895e6d5eb922acc303f71b9b160c77edbeda0f
Ing. Kamil Foltin <kamil.foltin@gyarab.cz>
2025-01-23 11:49:04 +0100

    articles

--- content/models.py
+++ content/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    perex = models.TextField()
+    text = models.TextField()
+    published = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
--- content/views.py
+++ content/views.py
@@ -2,18 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse as HTTPResponse
 from django.urls import reverse
 import json
+from .models import Article
 
 # Create your views here.
 def articles(request):
-    with open('articles.json', encoding="utf-8") as file:
-        data = json.load(file) 
+    articles = Article.objects.all()
     
-    return render(request, 'content/articles.html', {'data': data})
+    return render(request, 'content/articles.html', {'articles': articles})
 
 def article(request, id):
-    with open('articles.json', encoding="utf-8") as file:
-        data = json.load(file)
-     
-    article = data[id]
+    article = Article.objects.get(id=id)
+    
     return render(request, 'content/article.html', {'article': article})
     
